92c3484c69b84f1384f9db1f7530815d13458842
Alexander <Sakeran@gmail.com>
2022-06-24 13:19:10 -0400

    Install xstate

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { defineConfig } from "vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
+import replace from "@rollup/plugin-replace";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()]
-})
+  plugins: [
+    svelte(),
+    replace({
+      "process.env.NODE_ENV": process.env.NODE_ENV,
+      preventAssignment: true,
+    }),
+  ],
+});
