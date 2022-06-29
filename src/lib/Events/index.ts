df9bf21d16e743fd23c948085fad7ad162238bfc
Alexander <Sakeran@gmail.com>
2022-06-29 08:43:47 -0400

    Add simple event bus

---
+++ src/lib/Events/index.ts
@@ -0,0 +1,29 @@
+import { onDestroy } from "svelte";
+
+type EventCallback = (data?: any) => void;
+
+export class EventBus {
+  private listeners = new Map<string, Set<EventCallback>>();
+
+  on(eventName: string, callback: EventCallback) {
+    let listenerSet = this.listeners.get(eventName);
+    if (!listenerSet) {
+      listenerSet = new Set();
+      this.listeners.set(eventName, listenerSet);
+    }
+    listenerSet.add(callback);
+
+    onDestroy(() => {
+      listenerSet.delete(callback);
+    });
+  }
+
+  dispatch(eventName: string, data?) {
+    let listenerSet = this.listeners.get(eventName);
+    if (!listenerSet) return;
+
+    for (const cb of listenerSet) {
+      cb(data);
+    }
+  }
+}
