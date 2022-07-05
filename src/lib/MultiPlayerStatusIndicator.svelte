5305dd405cc0eee8945b9eee92fcafa9a04d3217
Alexander <Sakeran@gmail.com>
2022-07-05 11:18:56 -0400

    Improve Indicator sizing

--- src/lib/MultiPlayerStatusIndicator.svelte
+++ src/lib/MultiPlayerStatusIndicator.svelte
@@ -20,22 +20,22 @@
 <div class="basis-64">
   <div
     class:active-turn={activeTurn}
-    class="relative bg-blue-900 py-[0.625rem] md:p-4 lg:p-6 rounded-sm md:rounded-md flex flex-col lg:flex-row lg:justify-between lg:items-center text-center md:text-left motion-safe:transition-colors motion-safe:duration-700"
+    class="relative bg-blue-900 py-[0.625rem] md:py-3 md:px-4 lg:py-5 lg:px-6 rounded-sm md:rounded-md flex flex-col lg:flex-row lg:justify-between lg:items-center text-center md:text-left motion-safe:transition-colors motion-safe:duration-700"
   >
     <div
       style:--tail-offset={$tailOffset}
       class="tail absolute -z-10 [--tail-width:1rem] md:[--tail-width:1.5rem] lg:[--tail-width:2.5rem] w-[var(--tail-width)] aspect-square top-0 bg-blue-800 left-1/2 -translate-x-1/2 translate-y-[calc(var(--tail-width)_/_-2_*_var(--tail-offset))] rotate-45 motion-safe:transition-colors motion-safe:duration-500"
     />
     <span
-      class="text-sm text-blue-400 motion-safe:transition-colors motion-safe:duration-500 md:hidden"
+      class="text-sm text-blue-400 motion-safe:transition-colors motion-safe:duration-500 md:hidden leading-none"
       >P{playerId + 1}</span
     >
     <span
-      class="text-sm text-blue-400 motion-safe:transition-colors motion-safe:duration-500 hidden md:block lg:text-4.125"
+      class="text-sm text-blue-400 motion-safe:transition-colors motion-safe:duration-500 hidden md:block lg:text-4.125 leading-none"
       >Player {playerId + 1}</span
     >
     <span
-      class="text-blue-200 text-6 lg:text-8 motion-safe:transition-colors motion-safe:duration-500 "
+      class="text-blue-200 text-6 lg:text-8 motion-safe:transition-colors motion-safe:duration-500 lg:leading-none"
       >{$gameState.playerScores[playerId]}</span
     >
   </div>
